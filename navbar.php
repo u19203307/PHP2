78bca8160dfbc858e889713ce0296907a7df1ce2
u19203307 <88732282+u19203307@users.noreply.github.com>
2021-11-02 09:30:56 -0500

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,38 @@
+	<?php
+		if (isset($title))
+		{
+
+      $encabezado = 'Stock de Productos';
+      $navegacion = array('Inventario',' Categorías','Usuarios');
+      $opciones = array('Soporte','Salir');
+	?>
+<nav class="navbar navbar-default ">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#"><?php echo $encabezado?></a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="<?php if (isset($active_productos)){echo $active_productos;}?>"><a href="stock.php"><i class='glyphicon glyphicon-barcode'></i> <?php echo $navegacion[0]; ?></a></li>
+		<li class="<?php if (isset($active_categoria)){echo $active_categoria;}?>"><a href="categorias.php"><i class='glyphicon glyphicon-tags'></i><?php echo $navegacion[1]; ?></a></li>
+		<li class="<?php if (isset($active_usuarios)){echo $active_usuarios;}?>"><a href="usuarios.php"><i  class='glyphicon glyphicon-user'></i> <?php echo $navegacion[2]; ?></a></li>
+       </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="http://obedalvarado.pw/contacto/" target='_blank'><i class='glyphicon glyphicon-envelope'></i> <?php echo $opciones[0]; ?></a></li>
+		<li><a href="login.php?logout"><i class='glyphicon glyphicon-off'></i> <?php echo $opciones[1]; ?></a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+	<?php
+		}
+	?>
